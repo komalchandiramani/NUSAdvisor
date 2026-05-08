8814c7f2cfc6b49f1a48a57566525e9db5a0cca2
Komal Chandiramani <komal.chandiramani7@gmail.com>
2026-05-08 12:03:36 +0400

    edit: ignore .pem keys and allow CDN in CORS

--- backend/api.py
+++ backend/api.py
@@ -24,7 +24,7 @@ app = FastAPI(title="NUSAdvisor+", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://d2k55gvobdiojf.cloudfront.net"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
